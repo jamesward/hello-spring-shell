e8c46a92621eb1a2f3886ce166deb9f814f28b4d
James Ward <james@jamesward.com>
2025-12-28 16:30:45 -0700

    bump spring shell

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.0"
+    id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -10,7 +10,29 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.springframework.shell:spring-shell-starter-jansi:4.0.0-M2")
+    implementation("org.springframework.shell:spring-shell-starter-jansi:4.0.0-RC1")
+
+    /*
+    ***************************
+    APPLICATION FAILED TO START
+    ***************************
+
+    Description:
+
+    The Bean Validation API is on the classpath but no implementation could be found
+
+    Action:
+
+    Add an implementation, such as Hibernate Validator, to the classpath
+     */
+    runtimeOnly("org.hibernate.validator:hibernate-validator")
+
+    /*
+    java.lang.NoClassDefFoundError: jakarta/el/ELManager
+    at org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator.buildExpressionFactory(ResourceBundleMessageInterpolator.java:189) ~[hibernate-validator-9.0.1.Final.jar:9.0.1.Final]
+     */
+    runtimeOnly("org.glassfish:jakarta.el:4.0.2")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
